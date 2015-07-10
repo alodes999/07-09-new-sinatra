f04514cc66f356e9a8b2371cc42f4e7dc63c4c75
Andrew Lodes <alodes999@gmail.com>
2015-07-10 10:30:32 -0500

    Add documentation to products model

--- models/products.rb
+++ models/products.rb
@@ -3,6 +3,13 @@ require_relative 'database_instance_methods'
 
 class Product
   
+  attr_reader :id, :general_info, :product_specs, :where_to_buy
+  # This is the initialize method for Product Objects.  We have 4 attributes:
+  # 
+  # id =>  an Integer referring to the row id in our Database
+  # general_info => a String referring to the general_info column of the products table
+  # product_specs => a String referencing the product_specs column of the products table
+  # where_to_buy => a String referencing the where_to_buy column of our products table
   def initialize(args)
     @id = args[id]
     @general_info = args[general_info]
