709f92497e24fcf9ee9ec2d2a580a387fba9967b
Andrew Lodes <alodes999@gmail.com>
2015-07-10 14:29:41 -0500

    Add new views for add verification
    Change controller to account for them

--- controllers/home_controller.rb
+++ controllers/home_controller.rb
@@ -8,12 +8,21 @@ get "/list" do
   erb :"home/select"
 end
 
+get "/list_product_input" do
+  
+end
+
 get "/add" do
   erb :"add/add"
 end
 
 get "/product_add_to_database" do
-  
+  add_hash = params["product"]
+  if Product.add_with_name_val(add_hash)
+    "Success!"
+  else
+    "That didn't work, Product name already included"
+  end
 end
 
 get "/change" do
@@ -30,4 +39,8 @@ get "/delete" do
   @product = Product.all
   @menu_name = "Delete"
   erb :"home/select"
+end
+
+get "/delete_product_input" do
+  
 end
