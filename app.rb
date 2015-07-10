87e505d3959fb741e4da9703c086b0b5bbb21def
Andrew Lodes <alodes999@gmail.com>
2015-07-10 13:50:28 -0500

    Change app.rb with new controller/model info

--- app.rb
+++ app.rb
@@ -8,4 +8,6 @@ CONNECTION = SQLite3::Database.new("products.db")
 
 CONNECTION.execute("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, name TEXT, general_info TEXT, technical_specs TEXT, where_to_buy TEXT);")
 
-require_relative "controllers/home"
+require_relative "models/products"
+
+require_relative "controllers/home_controller"
